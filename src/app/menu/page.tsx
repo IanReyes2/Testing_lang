c732fc3a631b3b5125c67c5333b3c431514f5621
IanReyes2 <ian.vincent.reyes@adamson.edu.ph>
2025-09-05 20:09:33 +0800

    Awesomeness

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -26,8 +26,8 @@ export default function MenuPage() {
     async function fetchMenu() {
       try {
          const url = category
-          ? `http://192.168.100.27:3000/api/menu?category=${category}`
-          : `http://192.168.100.27:3000/api/menu`;
+          ? `http://192.168.1.5:3000/api/menu?category=${category}`
+          : `http://192.168.1.5:3000/api/menu`;
 
         const res = await fetch(url);
         const data = await res.json();
