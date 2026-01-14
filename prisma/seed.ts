067917166b0213c8dcd6b885968d668510e02ea1
IanReyes2 <ian.vincent.reyes@adamson.edu.ph>
2026-01-14 13:04:07 +0800

    working

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,7 +4,7 @@ const prisma = new PrismaClient();
 
 async function main() {
   // Add some menu items
-  await prisma.menuItem.createMany({
+  await prisma.menuitem.createMany({
     data: [
       { name: "Fried Chicken", price: "50.00" },
       { name: "Chicken Adobo", price: "50.00" },
